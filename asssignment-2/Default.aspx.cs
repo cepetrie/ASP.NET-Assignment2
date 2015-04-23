0bb864ac6ff26b73ebfceea667190360d7067010
cepetrie <christinapetrie@hotmail.com>
2015-04-23 13:11:10 -0700

    files for Assignment 2

---
+++ asssignment-2/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ShowTrackerEntities showentities = new ShowTrackerEntities();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        { 
+        var shows = from sa in showentities.Artists
+                    orderby sa.ArtistName
+                    select new { sa.ArtistName, sa.ArtistKey };
+
+        DropDownList1.DataSource = shows.ToList();
+        DropDownList1.DataTextField = "ArtistName";
+        DropDownList1.DataValueField = "ArtistKey";
+        DropDownList1.DataBind();
+        }
+
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int key = int.Parse(DropDownList1.SelectedValue.ToString());
+        var artist = from a in showentities.ShowDetails
+                     where a.ArtistKey == key
+                     select new {
+                     a.Show.ShowName,
+                     a.Show.ShowDate,
+                     a.Artist.ArtistName
+                     };
+        GridView1.DataSource = artist.ToList();
+        GridView1.DataBind();
+                     
+                    
+    }
+}
